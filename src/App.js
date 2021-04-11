c18917f3797cf3a7ede55c879203693e49f01697
alleemccoy <alleemccoy@hotmail.com>
2021-04-10 17:40:53 -0700

    Rewatching the lab 06 in Front Row and following along for better understanding and practice before moving on to lab 07, leaving off on Card 2 and 45 minutes into the recording

--- src/App.js
+++ src/App.js
@@ -9,37 +9,28 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state // need to finish this
-
     this.state = {
       city: ''
-      cityData: {}
     };
   }
-  handleFormSubmit = async(event) => {
+  handleFormSubmit = (event) => {
     event.preventDefault();
     console.log(this.state.city);
-    try {
-      let cityData = await axios.get ();
-    axios.get() //need to get this from LocationIQ - can find by rewatching recording
+    axios.get()
   }
-  render () {
+  render() {
     return (
       <>
         <h1>City Explorer</h1>
         <Form onSubmit={this.handleFormSubmit}>
           <Form.Group controlID="city">
-            <Form.Control value={this.state.city} onInput={e => this.setState({city: e.target.value})}></Form.Control>
+            <Form.Label>Enter city name</Form.Label>
+            <Form.Control value={this.state.city} onInput={event => this.setState({city: event.target.value})}></Form.Control>
           </Form.Group>
           <Button variant="primary" type="submit">
             Explore!
           </Button>
         </Form>
-        {this.state.cityData.lat !== undefined ? <Jumbotron>
-          <h3>{this.state.cityData.display_name}</h3>
-          <h5>{this.state.cityData.lat}, {this.stateCityData.lon}</h5>
-
-        </Jumbotron>}
       </>
     )
   }
